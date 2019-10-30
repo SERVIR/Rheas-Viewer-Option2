5bfbbbfb5dbbb07d3ba33706400689e43d4db0b4
gtondapu <githika.cs@gmail.com>
2019-10-30 15:08:10 -0500

    ignore added

--- tethysapp/rheasvieweroption2/config.py
+++ tethysapp/rheasvieweroption2/config.py
@@ -1,11 +1,11 @@
-connection = {'host': '10.5.5.31',
-              'user': 'rheas',
-              'password': 'servir@2018#',
-              'dbname': 'rheas'}
+connection = {'host': 'IP ADDRESS',
+              'user': 'USERNAME',
+              'password': 'PASS',
+              'dbname': 'DBNAME'}
 
-geoserver = {'rest_url':'http://tethys.servirglobal.net:8181/geoserver/rest',
-             'wms_url':'https://tethys.servirglobal.net/geoserver/wms',
-             'wfs_url':'https://tethys.servirglobal.net/geoserver/wfs',
-             'user':'admin',
-             'password':'geoserver',
-             'workspace':'darwin'}
+geoserver = {'rest_url':'REST URL',
+             'wms_url':'WMS URL',
+             'wfs_url':'WFS URL',
+             'user':'USERNAME',
+             'password':'PASS',
+             'workspace':'WORKSPACE'}
