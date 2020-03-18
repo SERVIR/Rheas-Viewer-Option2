832a962575204d918109b5fd98d2e395c4c07c6f
BillyZ <billy.ashmall@nasa.gov>
2020-03-18 20:23:57 +0300

    upgrade to V3

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 import os
 import sys
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_resource_files,custom_develop_command, custom_install_command
+from tethys_apps.app_installation import find_resource_files
 
 # -- Apps Definition -- #
 app_package = 'rheasvieweroption2'
 release_package = 'tethysapp-' + app_package
-app_class = 'rheasvieweroption2.app:Rheasvieweroption2'
-app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
-
 
 # -- Get Resource File -- #
 resource_files = find_resource_files('tethysapp/' + app_package + '/templates','tethysapp/' + app_package )
 resource_files += find_resource_files('tethysapp/' + app_package + '/public','tethysapp/' + app_package )
 
 # -- Python Dependencies -- #
-dependencies = []
+dependencies = ['xmltodict']
 
 setup(
     name=release_package,
@@ -28,14 +25,9 @@ setup(
     author_email='',
     url='',
     license='',
-    packages=find_namespace_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['tethysapp', 'tethysapp.' + app_package],
+    packages=find_namespace_packages(),
     include_package_data=True,
     package_data={'': resource_files},
     zip_safe=False,
     install_requires=dependencies,
-    cmdclass={
-        'install': custom_install_command(app_package, app_package_dir, dependencies),
-        'develop': custom_develop_command(app_package, app_package_dir, dependencies)
-    }
 )
